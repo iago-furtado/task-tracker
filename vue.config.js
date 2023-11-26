2a40ebbd35d693950305eab713232e7fc6d42a3a
iago-furtado <iago.rhf@outlook.com>
2023-11-25 20:52:39 -0500

    Fix the vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devserver: {
-    allowedhosts: "all"
+  transpileDependencies: ['vuetify'],
+  devServer: {
+    allowedHosts: ['*']
   }
 })
+
